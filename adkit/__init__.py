9413839c692044c7a9b2c15029ae9a7c7bcfdd52
fanyongtao <yongtao.fan@adleida.com>
2015-05-08 16:14:11 +0800

    build

---
+++ adkit/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# File Name: adkit/__init__.py
+# Author: fanyongtao
+# mail: jacketfan826@gmail.com
+# Created Time: 2015年05月08日 星期五 16时03分21秒
+#########################################################################
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+adkit
+-----
+
+adkit is an test tool for adexchange.
+
+"""
+
+import ast
+import os
+import os.path
+import re
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+data_dir = 'adkit/res'
+data = [os.path.join(data_dir, f) for f in os.listdir(data_dir)]
+
+with open('adkit/__init__.py', 'rb') as f:
+        version = str(ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1)))
+        setup(
+            name='adkit',
+            version=version,
+            url='http://git.adleida.com/paxp/',
+            author='adleida',
+            author_email='noreply@adleida.com',
+            description='adkit is an test tool for adexchange.',
+            long_description=__doc__,
+            packages=['adkit', 'adkit.ext'],
+            package_data={'adkit': data},
+            include_package_data=True,
+            zip_safe=False,
+            platforms='any',
+            entry_points='''
+                [console_scripts]
+                adkit=adkit.kit:main
+            ''',
+            install_requires=[
+                'jsonschema==2.4.0',
+                'PyYAML==3.11',
+                'requests==2.6.0',
+                'sh==1.11',
+                'termcolor==1.1.0'
+            ],
+        )
